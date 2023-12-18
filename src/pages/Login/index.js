154d9a2415e8009001bc4285c320ad8c294b9a32
臧国盛 <2356730090@qq.com>
2023-12-18 18:00:55 +0800

    登录-静态资源+表单校验+数据收集

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,51 @@
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
 
-function Login(){
+const Login = () => {
+    const onFinish = (values) =>{
+        console.log(values)
+    }
     return (
-        <div>this is a Login</div>
+        <div className="login">
+            <Card className="login-container">
+                <img className="login-logo" src={logo} alt="" />
+                {/* 登录表单 */}
+                <Form validateTrigger={['onBlur']} onFinish={onFinish}>
+                    <Form.Item
+                        name="mobile"
+                        rules={[
+                            {
+                                required: true,
+                                message: '手机号不能为空!',
+                            },
+                            {
+                                pattern: /^1[3-9]\d{9}$/,
+                                message: '手机号码格式错误!'
+                            }
+                        ]}
+                    >
+                        <Input size="large" placeholder="请输入手机号" />
+                    </Form.Item>
+                    <Form.Item
+                        name="code"
+                        rules={[
+                            {
+                                required: true,
+                                message: '验证码不能为空!',
+                            },
+                        ]}
+                    >
+                        <Input size="large" placeholder="请输入验证码" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" size="large" block>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
     )
 }
 
